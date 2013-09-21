c2d6e182915f6ead63260c28f81e68d6106fae3c
root <root@38-17-236-151.static.edis.at>
2013-09-21 10:16:16 +0200

    firewall settings added in essentials

--- modules/essential/manifests/init.pp
+++ modules/essential/manifests/init.pp
@@ -1,4 +1,12 @@
 class essential {
+
+	firewall { '100 allow http and https access':
+	    port   => [80, 443],
+	    proto  => tcp,
+	    action => accept,
+	    require => Class['firewall']
+	}
+
 	exec { 'apt-get update':
 	    command => "/usr/bin/apt-get update",
 	    onlyif => "/bin/bash -c 'exit $(( $(( $(date +%s) - $(stat -c %Y /var/lib/apt/lists/$( ls /var/lib/apt/lists/ -tr1|tail -1 )) )) <= 604800 ))'"
